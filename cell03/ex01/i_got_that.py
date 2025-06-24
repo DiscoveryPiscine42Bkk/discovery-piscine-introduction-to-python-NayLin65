f6f5cd6440ba487cdf7319a07f6ed0e23bdaa894
NayLin65 <66011645@kmitl.ac.th>
2025-06-24 19:25:32 +0700

    Create i_got_that.py

---
+++ cell03/ex01/i_got_that.py
@@ -0,0 +1,7 @@
+while True:
+        user_input = input("What you gotta say? : ")
+        
+        if user_input.upper() == "STOP":
+            break
+            
+        print("I got that! Anything else? : ", end="")
