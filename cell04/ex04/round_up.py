a613603f8cc536c5c4f98dfb39f0dc17aa1d2e25
NayLin65 <66011645@kmitl.ac.th>
2025-06-25 01:15:56 +0700

    Update round_up.py

--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -6,3 +6,6 @@ def round_up_number():
         print(f"Rounded up: {rounded}")
     except ValueError:
         print("Please enter a valid number")
+
+if __name__ == "__main__":
+    round_up_number()
