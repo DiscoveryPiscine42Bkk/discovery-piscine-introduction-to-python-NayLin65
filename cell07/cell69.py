85fd49038f9ba96dedb1a76c3914cd3f2ce668cd
NayLin65 <66011645@kmitl.ac.th>
2025-06-26 06:20:28 +0000

    123

---
+++ cell07/cell69.py
@@ -0,0 +1,8 @@
+import sys 
+if len(sys.argv) <2 :
+    print("none")
+else:
+    for i in range(1, len(sys.argv)):
+       
+        if "ism" not in sys.argv[i]:
+            print(f"{sys.argv[i]}ism")
